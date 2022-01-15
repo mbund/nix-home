a7f0168f3d9b3d6f7db53bab16622083775f08ff
mbund <bundschuhmark@gmail.com>
2022-01-14 23:07:31 -0500

    set up xdg directories

--- common/flake.nix
+++ common/flake.nix
@@ -3,6 +3,21 @@
 
   outputs = { self, ... }: {
     home = { config, lib, pkgs, ... }: {
+      xdg.userDirs = {
+        enable = true;
+        desktop = "$HOME/xdg/desktop";
+        documents = "$HOME/xdg/documents";
+        download = "$HOME/xdg/downloads";
+        music = "$HOME/xdg/music";
+        pictures = "$HOME/xdg/pictures";
+        publicShare = "$HOME/xdg/public";
+        templates = "$HOME/xdg/templates";
+        videos = "$HOME/xdg/videos";
+        extraConfig = {
+          XDG_MISC_DIR = "$HOME/xdg/misc";
+        };
+      };
+
       programs.home-manager.enable = true;
     };
   };
