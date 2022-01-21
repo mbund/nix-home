26b2c25ae90f1d106279399628da92983ea88c5a
= <bundschuhmark@gmail.com>
2022-01-21 17:14:17 -0500

    Edit KDE plasma settings

--- plasma/flake.nix
+++ plasma/flake.nix
@@ -45,25 +45,16 @@
               Id_2 = "6ccf0ea5-46df-4f08-ab39-b1fa4746ca49";
               Id_3 = "2e95628f-98b9-4a00-96dd-d2184b8083c2";
               Id_4 = "0a26cb7c-f464-4c43-b14a-46bb2f29ebad";
-              Id_5 = "b1a433a5-224c-431b-be35-fd73ec5422a9";
-              Id_6 = "c054f6b4-e785-41db-b95f-3f01afbd0e81";
-              Id_7 = "002ae383-0bcc-4b9b-864b-2f941eba1e85";
-              Id_8 = "5a94e647-97db-4177-ac52-39a57835c49f";
-              Id_9 = "6a018ee5-7537-4f68-a85e-bafc4c91eec5";
               Name_1 = "1";
               Name_2 = "2";
               Name_3 = "3";
               Name_4 = "4";
-              Name_5 = "5";
-              Name_6 = "6";
-              Name_7 = "7";
-              Name_8 = "8";
-              Name_9 = "9";
-              Number = 9;
-              Rows = 3;
+              Number = 4;
+              Rows = 1;
             };
             Windows = {
               RollOverDesktops = false;
+              BorderlessMaximizedWindows = false;
             };
           };
           kdeglobals.KDE.SingleClick = false;
